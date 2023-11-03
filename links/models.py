709a53f83fead4b1a3cc302c136288ec1fd52336
bkleinb78 <y2bk728@aol.com>
2023-11-02 19:00:52 -0500

    created the urls.py file in the links app

--- links/models.py
+++ links/models.py
@@ -1,20 +1,21 @@
 from django.db import models
-from django.utils import slugify
+from django.utils.text import slugify
 
-# Create your models here.
 class Link(models.Model):
-    name = models.CharField(max_legnth=50, unique=True)
+    name = models.CharField(max_length=50, unique=True)
     url = models.URLField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
-    clicks= models.PositiveIntegerField(default=0)
+    clicks = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.name} | {self.clicks}"
-    
+
     def click(self):
-        self.clicks +=1
+        self.clicks += 1
         self.save()
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug= slugify(self.name)
+            self.slug = slugify(self.name)
+
+        return super().save(*args, **kwargs)
