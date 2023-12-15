873e2f99635b6a517fc2b081a886ad4c52e8d7eb
bkleinb78 <y2bk728@aol.com>
2023-12-14 23:59:38 -0600

    Added the models, url files along with making a form for the project

--- links/models.py
+++ links/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 class Link(models.Model):
     name = models.CharField(max_length=50, unique=True)
     url = models.URLField(max_length=200)
--- links/urls.py
+++ links/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name="home"),
     path('<str:link_slug>/', views.root_link, name="root-link"),
